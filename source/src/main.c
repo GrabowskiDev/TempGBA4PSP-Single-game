8e10b27e3b904e37df0519770edee7e9a1032ff4
Łukasz Grabowski <lukaszgra2004@gmail.com>
2023-01-12 22:05:05 +0100

    Update main.c

--- source/src/main.c
+++ source/src/main.c
@@ -790,7 +790,7 @@ int user_main(int argc, char *argv[])
 
   gamepak_filename[0] = 0;
 
-  strcpy(load_filename, "roms/game.gba");
+  strcpy(load_filename, "game");
   load_gamepak(load_filename);
 
   if (argc > 1)
